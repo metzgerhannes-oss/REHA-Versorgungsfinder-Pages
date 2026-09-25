b93644024a38f8b2de1a797396ab832b7d42c4dc
metzgerhannes-oss <metzger.hannes@googlemail.com>
2026-09-25 10:29:31 +0200

    Service Worker Cache auf v19.4 umstellen

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='reha-v19-secure-v19-3';
+const CACHE='reha-v19-secure-v19-4';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',event=>{
